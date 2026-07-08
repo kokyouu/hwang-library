4c9aca6eb3d3456f317a5a53cfdfd4e095199704
wang-hanworkhome <1265096439@qq.com>
2026-07-08 20:38:31 +0800

    Create basic user information form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
-import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-  <JSON />
-</template>
-
-<style scoped>
-</style>
+  <Form />
+  </template>
