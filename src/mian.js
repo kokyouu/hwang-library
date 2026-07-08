b81093e2e07546f6a364fb4e92b778fd2e1e7d86
wang-hanworkhome <1265096439@qq.com>
2026-07-08 20:41:11 +0800

    Set up Bootstrap 5

--- src/mian.js
+++ src/mian.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
-import './style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// import './style.css'
 
 createApp(App).mount('#app')
