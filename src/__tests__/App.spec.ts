57205e71dca91256c4bd8c6e90b7147e37c88115
wang-hanworkhome <1265096439@qq.com>
2026-07-30 20:18:12 +0800

    Set up Lab 12 GitHub Pages deployment

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -4,8 +4,8 @@ import { mount } from '@vue/test-utils'
 import App from '../App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
+  it('renders the user information form', () => {
     const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+    expect(wrapper.get('h1').text()).toBe('User Information Form')
   })
 })
